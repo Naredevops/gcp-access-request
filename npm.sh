bb4b0f37c41455e020b3f5ebab662189084ed95d
Naredevops <48598624+Naredevops@users.noreply.github.com>
2024-01-02 15:35:55 +0530

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,4 +1,6 @@
 npm install
 npm install --save-dev webpack webpack-cli
+npx webpack --version
+npm install webpack@latest webpack-cli@latest --save-dev
 npm run build
 cat /home/runner/.npm/_logs/2024-01-02T09_34_55_967Z-debug.log
