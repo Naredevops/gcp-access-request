7e5ba383e678e6f880dd28eb008651fbad780966
Naredevops <48598624+Naredevops@users.noreply.github.com>
2024-01-02 17:32:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-mkdir src
-touch src/index.js
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = 'src';
+const filePath = path.join(directoryPath, 'index.js');
+
+// Create the 'src' directory
+fs.mkdirSync(directoryPath);
+
+// Create the 'index.js' file
+fs.writeFileSync(filePath, '');
+
+console.log('Directory and file created successfully.');
