83e4cc3e06bd97dfd7ca9dbba832ecf827ad82fc
Naredevops <48598624+Naredevops@users.noreply.github.com>
2024-01-02 15:04:33 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,3 +2,6 @@ module.exports = {
   entry: './src/index.js', // or the path to your actual entry file
   // other webpack configurations...
 };
+console.log('Current directory content:', __dirname);
+console.log('Webpack configuration:', module.exports);
+
